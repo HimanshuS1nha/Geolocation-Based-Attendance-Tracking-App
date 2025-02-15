f43fcaf5ef63d10751bde25b99f87749d7aa3fc1
Himanshu Sinha <ashusinha1904@gmail.com>
2025-02-15 21:09:50 +0530

    Socket.io setup complete

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
 const port = process.env.PORT || 8000;
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id} Joined`);
+});
 
 app.get("/", (_, res) => {
   res.send("Hello World");
   return;
 });
 
-app.listen(port, () => console.log(`Listenting on http://localhost:${port}`));
+server.listen(port, () =>
+  console.log(`Listenting on http://localhost:${port}`)
+);
