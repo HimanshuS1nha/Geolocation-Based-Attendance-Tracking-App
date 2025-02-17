cc6edd1ea17f3a351ee709226537d030806eb5df
Himanshu Sinha <ashusinha1904@gmail.com>
2025-02-17 18:59:18 +0530

    signup page UI complete

--- client/app/auth/signup.tsx
+++ client/app/auth/signup.tsx
@@ -1,6 +1,8 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Image, Pressable } from "react-native";
 import React, { useState, useCallback } from "react";
 import tw from "twrnc";
+import * as ImagePicker from "expo-image-picker";
+import { FontAwesome, AntDesign, MaterialIcons } from "@expo/vector-icons";
 
 import Input from "@/components/Input";
 import Button from "@/components/Button";
@@ -31,6 +33,21 @@ const Signup = () => {
     },
     []
   );
+
+  const pickImage = useCallback(async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+      base64: true,
+    });
+
+    if (!result.canceled) {
+      setFileName(result.assets[0].fileName || "");
+      setFileBase64(result.assets[0].base64 || "");
+    }
+  }, []);
   return (
     <View style={tw`flex-1 bg-white`}>
       <ScrollView contentContainerStyle={tw`pb-4 px-4 gap-y-9`}>
@@ -39,6 +56,35 @@ const Signup = () => {
         </Text>
 
         <View style={tw`gap-y-3.5 items-center`}>
+        {fileBase64 ? (
+          <View style={tw`items-center justify-center`}>
+            <Image
+              source={{
+                uri: `data:image/png;base64,${fileBase64}`,
+              }}
+              style={tw`size-28 rounded-full`}
+            />
+            <Pressable
+              style={tw`absolute bg-emerald-600 bottom-0 right-0 p-2 rounded-full z-10 shadow shadow-black`}
+              onPress={pickImage}
+            >
+              <MaterialIcons name="change-circle" size={26} color="white" />
+            </Pressable>
+          </View>
+        ) : (
+          <View
+            style={tw`size-28 rounded-full bg-gray-300 items-center justify-center`}
+          >
+            <FontAwesome name="user" size={60} color={"white"} />
+            <Pressable
+              style={tw`absolute bg-emerald-600 bottom-0 right-0 p-2 rounded-full z-10 shadow shadow-black`}
+              onPress={pickImage}
+            >
+              <AntDesign name="plus" size={26} color="white" />
+            </Pressable>
+          </View>
+        )}
+
           <Input
             placeholder="Enter company's name"
             value={name}
