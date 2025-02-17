c45f53411e595f3d36f0449d111d773ab1ff4019
Himanshu Sinha <ashusinha1904@gmail.com>
2025-02-17 22:06:50 +0530

    Tabs layout updated

--- client/app/company/(tabs)/_layout.tsx
+++ client/app/company/(tabs)/_layout.tsx
@@ -1,11 +1,49 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import { Pressable, Image } from "react-native";
+import tw from "twrnc";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
+
+import { useUser } from "@/hooks/useUser";
 
 const CompanyTabsLayout = () => {
+  const user = useUser((state) => state.user);
   return (
-    <Tabs>
-      <Tabs.Screen name="home" />
-      <Tabs.Screen name="employees" />
+    <Tabs
+      screenOptions={{
+        headerTitle: "GeoAttendance",
+        headerRight: () => {
+          return (
+            <Pressable>
+              <Image
+                source={{
+                  uri: user?.image,
+                }}
+                style={tw`size-9 rounded-full mr-2`}
+              />
+            </Pressable>
+          );
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return <Ionicons name="home-sharp" size={size} color={color} />;
+          },
+          title: "Home",
+        }}
+      />
+      <Tabs.Screen
+        name="employees"
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return <FontAwesome name="users" size={size} color={color} />;
+          },
+          title: "Employees",
+        }}
+      />
     </Tabs>
   );
 };
