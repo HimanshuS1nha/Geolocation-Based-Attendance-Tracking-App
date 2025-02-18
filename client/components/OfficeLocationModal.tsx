7e83a00efdf2c795ba072ffee70ebfd265eb2da3
Himanshu Sinha <ashusinha1904@gmail.com>
2025-02-18 22:03:06 +0530

    Office location modal complete

--- client/components/OfficeLocationModal.tsx
+++ client/components/OfficeLocationModal.tsx
@@ -1,12 +1,19 @@
-import { View, Text, Modal, Pressable } from "react-native";
+import { View, Text, Modal, Pressable, Alert } from "react-native";
 import React, { useState, useCallback } from "react";
 import tw from "twrnc";
 import { Entypo } from "@expo/vector-icons";
+import * as SecureStore from "expo-secure-store";
+import * as Location from "expo-location";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { ZodError } from "zod";
 
-import Input from "./Input";
-import Button from "./Button";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
 
 import { useOfficeLocationModal } from "@/hooks/useOfficeLocationModal";
+
+import { addOfficeLocatioValidator } from "@/validators/add-office-location-validator";
 
 const OfficeLocationModal = () => {
   const { isVisible, setIsVisible } = useOfficeLocationModal();
@@ -24,6 +31,65 @@ const OfficeLocationModal = () => {
     },
     []
   );
+
+  const { mutate: handleAddOfficeLocation, isPending } = useMutation({
+    mutationKey: ["add-office-location"],
+    mutationFn: async () => {
+      const token = SecureStore.getItem("token");
+      if (!token) {
+        throw new Error("Please login first");
+      }
+
+      const parsedData = await addOfficeLocatioValidator.parseAsync({
+        latitude: parseFloat(latitude),
+        longitude: parseFloat(longitude),
+      });
+
+      const { data } = await axios.post(
+        `${process.env.EXPO_PUBLIC_API_URL}/api/add-office-location`,
+        { ...parsedData },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+
+      return data as { message: string };
+    },
+    onSuccess: (data) => {
+      Alert.alert("Success", data.message, [
+        {
+          text: "Ok",
+          onPress: () => setIsVisible(false),
+        },
+      ]);
+    },
+    onError: (error) => {
+      if (error instanceof ZodError) {
+        Alert.alert("Error", error.errors[0].message);
+      } else if (error instanceof AxiosError && error.response?.data.error) {
+        Alert.alert("Error", error.response.data.error);
+      } else {
+        Alert.alert("Error", error.message);
+      }
+    },
+  });
+
+  const handleUseCurrentLocation = useCallback(async () => {
+    const permission = await Location.getForegroundPermissionsAsync();
+    if (permission.granted) {
+      const location = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.Highest,
+      });
+
+      setLatitude(location.coords.latitude.toString());
+      setLongitude(location.coords.longitude.toString());
+
+      handleAddOfficeLocation();
+    } else {
+      Alert.alert(
+        "Error",
+        "Location permission is required for this feature to work."
+      );
+    }
+  }, []);
   return (
     <Modal visible={isVisible} transparent animationType="fade">
       <View style={tw`flex-1 justify-center items-center bg-gray-100/70`}>
@@ -57,8 +123,10 @@ const OfficeLocationModal = () => {
           </View>
 
           <View style={tw`gap-y-3 w-full`}>
-            <Button>Add/Update Location</Button>
-            <Button variant="outline">Use My Current Location</Button>
+            <Button disabled={isPending}>Add/Update Location</Button>
+            <Button variant="outline" disabled={isPending}>
+              Use My Current Location
+            </Button>
           </View>
         </View>
       </View>
