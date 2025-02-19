a51ee3bf9fb8ce9e1fe28cde4e38b5f722f76772
Himanshu Sinha <ashusinha1904@gmail.com>
2025-02-19 22:39:42 +0530

    Edit employee profile page complete

--- client/app/employee/edit-profile.tsx
+++ client/app/employee/edit-profile.tsx
@@ -1,12 +1,19 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Alert } from "react-native";
 import React, { useState, useCallback } from "react";
 import tw from "twrnc";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { ZodError } from "zod";
+import * as SecureStore from "expo-secure-store";
+import { router } from "expo-router";
 
 import ImagePicker from "@/components/ImagePicker";
 import Input from "@/components/Input";
 import Button from "@/components/Button";
 
 import { useUser } from "@/hooks/useUser";
+
+import { editEmployeeProfileValidator } from "@/validators/edit-profile-validator";
 
 const EditEmployeeProfile = () => {
   const user = useUser((state) => state.user);
@@ -26,6 +33,50 @@ const EditEmployeeProfile = () => {
     },
     []
   );
+
+  const { mutate: handleEditProfile, isPending } = useMutation({
+    mutationKey: ["edit-profile"],
+    mutationFn: async () => {
+      const token = SecureStore.getItem("token");
+      if (!token) {
+        throw new Error("Please login first");
+      }
+
+      const parsedData = await editEmployeeProfileValidator.parseAsync({
+        name,
+        fileName,
+        fileBase64,
+        designation,
+      });
+
+      const { data } = await axios.post(
+        `${process.env.EXPO_PUBLIC_API_URL}/api/edit-profile/employee`,
+        { ...parsedData },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      return data as { message: string };
+    },
+    onSuccess: async (data) => {
+      Alert.alert("Success", data.message, [
+        {
+          text: "Ok",
+          onPress: router.back,
+        },
+      ]);
+    },
+    onError: (error) => {
+      if (error instanceof ZodError) {
+        Alert.alert("Error", error.errors[0].message);
+      } else if (error instanceof AxiosError && error.response?.data.error) {
+        Alert.alert("Error", error.response.data.error);
+      } else {
+        Alert.alert("Error", error.message);
+      }
+    },
+  });
   return (
     <View style={tw`flex-1 bg-white`}>
       <ScrollView contentContainerStyle={tw`pt-6 px-4 gap-y-6 items-center`}>
@@ -47,7 +98,9 @@ const EditEmployeeProfile = () => {
           onChangeText={(text) => handleChangeText("designation", text)}
         />
 
-        <Button>Edit Profile</Button>
+        <Button onPress={handleEditProfile} disabled={isPending}>
+          Edit Profile
+        </Button>
       </ScrollView>
     </View>
   );
--- client/validators/edit-profile-validator.ts
+++ client/validators/edit-profile-validator.ts
@@ -8,3 +8,16 @@ export const editCompanyProfileValidator = z.object({
   fileName: z.string().optional(),
   fileBase64: z.string().optional(),
 });
+
+export const editEmployeeProfileValidator = z.object({
+  name: z
+    .string({ required_error: "Name is required" })
+    .trim()
+    .min(1, { message: "Name is required" }),
+  designation: z
+    .string({ required_error: "Designation is required" })
+    .trim()
+    .min(1, { message: "Designation is required" }),
+  fileName: z.string().optional(),
+  fileBase64: z.string().optional(),
+});
