1325cbd4be1d5233cfb64e136d55b935d55fb057
Himanshu Sinha <ashusinha1904@gmail.com>
2025-02-19 22:24:57 +0530

    Socket functionality implemented for company

--- client/app/company/(tabs)/home.tsx
+++ client/app/company/(tabs)/home.tsx
@@ -1,11 +1,56 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Alert } from "react-native";
+import React, { useState, useCallback, useEffect } from "react";
 import tw from "twrnc";
+import * as SecureStore from "expo-secure-store";
 
 import ProfileCard from "@/components/ProfileCard";
 import Button from "@/components/Button";
 
+import { useUser } from "@/hooks/useUser";
+import { useOfficeLocationModal } from "@/hooks/useOfficeLocationModal";
+
+import { socket } from "@/libs/socket";
+
 const CompanyHome = () => {
+  const user = useUser((state) => state.user);
+  const setIsOfficeLocationModalVisible = useOfficeLocationModal(
+    (state) => state.setIsVisible
+  );
+
+  const [isTakingAttendance, setIsTakingAttendance] = useState(
+    socket.connected
+  );
+
+  const handleStartTakingAttendance = useCallback(() => {
+    if (!user?.hasAddedOfficeLocation) {
+      return setIsOfficeLocationModalVisible(true);
+    }
+
+    const token = SecureStore.getItem("token");
+    socket.auth = { token };
+
+    socket.connect();
+    setIsTakingAttendance(true);
+  }, [user]);
+
+  const handleStopTakingAttendance = useCallback(() => {
+    socket.disconnect();
+    setIsTakingAttendance(false);
+  }, []);
+
+  const handleSocketError = useCallback(({ error }: { error: string }) => {
+    Alert.alert("Error", error);
+  }, []);
+
+  useEffect(() => {
+    if (socket.connected) {
+      socket.on("error", handleSocketError);
+    }
+
+    return () => {
+      socket.off("error", handleSocketError);
+    };
+  }, [socket.connected]);
   return (
     <View style={tw`flex-1`}>
       <View style={tw`flex-1 pt-4 pb-8 px-4 gap-y-7`}>
@@ -13,7 +58,18 @@ const CompanyHome = () => {
       </View>
 
       <View style={tw`pb-2 px-2 items-center`}>
-        <Button>Start Taking Attendance</Button>
+        <Button
+          style={tw`${isTakingAttendance ? "bg-rose-600" : "bg-indigo-600"}`}
+          onPress={
+            isTakingAttendance
+              ? handleStopTakingAttendance
+              : handleStartTakingAttendance
+          }
+        >
+          {isTakingAttendance
+            ? "Stop Taking Attendance"
+            : "Start Taking Attendance"}
+        </Button>
       </View>
     </View>
   );
