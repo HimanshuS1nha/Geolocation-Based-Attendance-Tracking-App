bbe3e0162b080ece62d57870680262bbe69370c2
Himanshu Sinha <ashusinha1904@gmail.com>
2025-02-19 22:10:37 +0530

    Employee layout updated

--- client/app/employee/_layout.tsx
+++ client/app/employee/_layout.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 import { Stack } from "expo-router";
+import { Pressable, Image } from "react-native";
+import tw from "twrnc";
+
+import { useUser } from "@/hooks/useUser";
 
 const EmployeeStackLayout = () => {
-  return <Stack />;
+  const user = useUser((state) => state.user);
+  return (
+    <Stack>
+      <Stack.Screen
+        name="home"
+        options={{
+          headerTitle: "GeoAttendance",
+          headerRight: () => {
+            return (
+              <Pressable>
+                <Image
+                  source={{
+                    uri: user?.image,
+                  }}
+                  style={tw`size-9 rounded-full mr-2`}
+                />
+              </Pressable>
+            );
+          },
+        }}
+      />
+    </Stack>
+  );
 };
 
 export default EmployeeStackLayout;
