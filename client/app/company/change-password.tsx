b924967114edb96aee371d2dc789f8f183c51a08
Himanshu Sinha <ashusinha1904@gmail.com>
2025-02-20 15:05:19 +0530

    Change company password page complete

--- client/app/company/change-password.tsx
+++ client/app/company/change-password.tsx
@@ -1,9 +1,16 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, ScrollView, Alert } from "react-native";
 import React, { useState, useCallback } from "react";
 import tw from "twrnc";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import * as SecureStore from "expo-secure-store";
+import { router } from "expo-router";
+import { ZodError } from "zod";
 
 import Input from "@/components/Input";
 import Button from "@/components/Button";
+
+import { changePasswordValidator } from "@/validators/change-password-validator";
 
 const ChangeCompanyPassword = () => {
   const [oldPassword, setOldPassword] = useState("");
@@ -25,6 +32,54 @@ const ChangeCompanyPassword = () => {
     },
     []
   );
+
+  const { mutate: handleChangePassword, isPending } = useMutation({
+    mutationKey: ["change-password"],
+    mutationFn: async () => {
+      const token = SecureStore.getItem("token");
+      if (!token) {
+        throw new Error("Please login first");
+      }
+
+      const parsedData = await changePasswordValidator.parseAsync({
+        oldPassword,
+        newPassword,
+        confirmPassword,
+      });
+      if (parsedData.newPassword !== parsedData.confirmPassword) {
+        throw new Error("Passwords do not match");
+      }
+
+      const { data } = await axios.post(
+        `${process.env.EXPO_PUBLIC_API_URL}/api/change-password/company`,
+        { ...parsedData },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      return data as { message: string };
+    },
+    onSuccess: (data) => {
+      Alert.alert("Success", data.message, [
+        {
+          text: "Ok",
+          onPress: router.back,
+        },
+      ]);
+    },
+    onError: (error) => {
+      if (error instanceof ZodError) {
+        Alert.alert("Error", error.errors[0].message);
+      } else if (error instanceof AxiosError && error.response?.data.error) {
+        Alert.alert("Error", error.response.data.error);
+      } else {
+        Alert.alert("Error", error.message);
+      }
+    },
+  });
   return (
     <View style={tw`flex-1 bg-white`}>
       <ScrollView contentContainerStyle={tw`pt-6 px-4 gap-y-6 items-center`}>
@@ -47,7 +102,9 @@ const ChangeCompanyPassword = () => {
           onChangeText={(text) => handleChangeText("confirmPassword", text)}
         />
 
-        <Button>Change Password</Button>
+        <Button onPress={handleChangePassword} disabled={isPending}>
+          Change Password
+        </Button>
       </ScrollView>
     </View>
   );
---
+++ client/validators/change-password-validator.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const changePasswordValidator = z.object({
+  oldPassword: z
+    .string({ required_error: "Old Password is required" })
+    .trim()
+    .min(8, { message: "Old Password must be atleast 8 characters long" }),
+  newPassword: z
+    .string({ required_error: "New Password is required" })
+    .trim()
+    .min(8, { message: "New Password must be atleast 8 characters long" }),
+  confirmPassword: z
+    .string({ required_error: "Confirm Password is required" })
+    .trim()
+    .min(8, { message: "Confirm Password must be atleast 8 characters long" }),
+});
