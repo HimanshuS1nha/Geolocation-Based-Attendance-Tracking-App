87dcfe2e79c747772e2efdefd51cab024c2cd6ab
Himanshu Sinha <ashusinha1904@gmail.com>
2025-02-21 14:57:56 +0530

    Edit employee page complete

--- client/app/company/edit-employee.tsx
+++ client/app/company/edit-employee.tsx
@@ -1,11 +1,18 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Alert } from "react-native";
 import React, { useState, useCallback } from "react";
 import tw from "twrnc";
 import { useLocalSearchParams } from "expo-router";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { ZodError } from "zod";
+import * as SecureStore from "expo-secure-store";
+import { router } from "expo-router";
 
 import ImagePicker from "@/components/ImagePicker";
 import Input from "@/components/Input";
 import Button from "@/components/Button";
+
+import { editEmployeeValidator } from "@/validators/edit-employee-validator";
 
 import type { EmployeeType } from "@/types";
 
@@ -31,6 +38,52 @@ const EditEmployee = () => {
     },
     []
   );
+
+  const { mutate: handleEditEmployee, isPending } = useMutation({
+    mutationKey: ["edit-employee"],
+    mutationFn: async () => {
+      const token = SecureStore.getItem("token");
+      if (!token) {
+        throw new Error("Please login first");
+      }
+
+      const parsedData = await editEmployeeValidator.parseAsync({
+        name: newName,
+        fileName,
+        fileBase64,
+        email: newEmail,
+        designation: newDesignation,
+      });
+
+      const { data } = await axios.post(
+        `${process.env.EXPO_PUBLIC_API_URL}/api/edit-employee/${id}`,
+        { ...parsedData },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      return data as { message: string };
+    },
+    onSuccess: async (data) => {
+      //! Invalidate get employees call
+      Alert.alert("Success", data.message, [
+        {
+          text: "Ok",
+          onPress: router.back,
+        },
+      ]);
+    },
+    onError: (error) => {
+      if (error instanceof ZodError) {
+        Alert.alert("Error", error.errors[0].message);
+      } else if (error instanceof AxiosError && error.response?.data.error) {
+        Alert.alert("Error", error.response.data.error);
+      } else {
+        Alert.alert("Error", error.message);
+      }
+    },
+  });
   return (
     <View style={tw`flex-1 bg-white`}>
       <ScrollView contentContainerStyle={tw`pt-6 px-4 gap-y-6 items-center`}>
@@ -58,7 +111,9 @@ const EditEmployee = () => {
           onChangeText={(text) => handleChangeText("designation", text)}
         />
 
-        <Button>Edit Employee</Button>
+        <Button onPress={handleEditEmployee} disabled={isPending}>
+          Edit Employee
+        </Button>
       </ScrollView>
     </View>
   );
