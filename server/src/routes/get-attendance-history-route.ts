9566b88890cad53d292834358b1a932c865c4923
Himanshu Sinha <ashusinha1904@gmail.com>
2025-02-23 02:36:17 +0530

    /api/get-attendance-history/company/:employeeId route added

--- server/src/routes/get-attendance-history-route.ts
+++ server/src/routes/get-attendance-history-route.ts
@@ -96,6 +96,122 @@ getAttendanceHistoryRouter.post("/company", async (req, res) => {
   }
 });
 
+getAttendanceHistoryRouter.post("/company/:employeeId", async (req, res) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    if (!authHeader) {
+      res.status(401).json({ error: "Unauthorized" });
+      return;
+    }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      res.status(401).json({ error: "Unauthorized" });
+      return;
+    }
+
+    const { email, type } = jwt.verify(token, process.env.JWT_SECRET!) as {
+      email: string;
+      type: "company";
+    };
+    if (type !== "company" || !email) {
+      res.status(401).json({ error: "Unauthorized" });
+      return;
+    }
+
+    const company = await getCompanyByEmail(email);
+    if (!company) {
+      res.status(401).json({ error: "Unauthorized" });
+      return;
+    }
+
+    const { employeeId } = req.params;
+    const employee = await prisma.employees.findUnique({
+      where: {
+        id: employeeId,
+        companyEmail: company.email,
+      },
+    });
+    if (!employee) {
+      res.status(404).json({ error: "Employee not found" });
+      return;
+    }
+
+    const { skip, take } = await getAttendanceHistoryValidator.parseAsync(
+      req.body
+    );
+
+    const attendanceRecords: {
+      date: string;
+      timeSpent: string;
+      isPresent: boolean;
+      day: number;
+    }[] = [];
+    const numberOfEntriesToTake = take ?? 20;
+    const numberOfEntriesToSkip = skip ?? 0;
+
+    for (let i = 0; i <= numberOfEntriesToTake; i++) {
+      const currentDate = new Date();
+      currentDate.setDate(new Date().getDate() - (numberOfEntriesToSkip + i));
+
+      const employeeEntries = await prisma.employeeEntry.findMany({
+        where: {
+          attendance: {
+            date: currentDate.getDate(),
+            month: currentDate.getMonth(),
+            year: currentDate.getFullYear(),
+            employeeEmail: employee.email,
+          },
+        },
+        select: {
+          EmployeeExit: {
+            select: {
+              exitedAt: true,
+            },
+          },
+          enteredAt: true,
+        },
+      });
+
+      let timeSpentInSeconds = 0;
+
+      for (const employeeEntry of employeeEntries) {
+        if (employeeEntry.enteredAt && employeeEntry.EmployeeExit?.exitedAt) {
+          timeSpentInSeconds +=
+            (employeeEntry.EmployeeExit.exitedAt.getTime() -
+              employeeEntry.enteredAt.getTime()) /
+            1000;
+        }
+      }
+
+      attendanceRecords.push({
+        timeSpent: `${String(Math.floor(timeSpentInSeconds / 3600)).padStart(
+          2,
+          "0"
+        )}:${String(Math.floor((timeSpentInSeconds % 3600) / 60)).padStart(
+          2,
+          "0"
+        )}`,
+        date: `${String(currentDate.getDate()).padStart(2, "0")}/${String(
+          currentDate.getMonth() + 1
+        ).padStart(2, "0")}/${currentDate.getFullYear()}`,
+        day: currentDate.getDay(),
+        isPresent: employeeEntries.length > 0,
+      });
+    }
+
+    res.status(200).json({ attendanceRecords });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      res.status(422).json({ error: error.errors[0].message });
+    } else {
+      res
+        .status(500)
+        .json({ error: "Some error occured. Please try again later!" });
+    }
+  }
+});
+
 getAttendanceHistoryRouter.post("/employee", async (req, res) => {
   try {
     const authHeader = req.headers["authorization"];
@@ -132,7 +248,7 @@ getAttendanceHistoryRouter.post("/employee", async (req, res) => {
     const attendanceRecords: {
       date: string;
       day: number;
-      present: boolean;
+      isPresent: boolean;
     }[] = [];
     const numberOfEntriesToTake = take ?? 20;
     const numberOfEntriesToSkip = skip ?? 0;
@@ -152,7 +268,7 @@ getAttendanceHistoryRouter.post("/employee", async (req, res) => {
       });
 
       attendanceRecords.push({
-        present: employeeEntries.length > 0,
+        isPresent: employeeEntries.length > 0,
         date: `${String(currentDate.getDate()).padStart(2, "0")}/${String(
           currentDate.getMonth() + 1
         ).padStart(2, "0")}/${currentDate.getFullYear()}`,
