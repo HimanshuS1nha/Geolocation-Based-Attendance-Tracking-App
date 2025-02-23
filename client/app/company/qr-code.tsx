ebdb50894005f029772818d29da260d84cb73d4d
Himanshu Sinha <ashusinha1904@gmail.com>
2025-02-23 14:48:16 +0530

    QR code screen complete

--- client/app/company/qr-code.tsx
+++ client/app/company/qr-code.tsx
@@ -1,21 +1,59 @@
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator, Alert } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import QRCode from "react-native-qrcode-svg";
+import { useQuery } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import * as SecureStore from "expo-secure-store";
 
 const QrCode = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["get-qr-code"],
+    queryFn: async () => {
+      const token = SecureStore.getItem("token");
+      if (!token) {
+        throw new Error("Please login first");
+      }
+
+      const { data } = await axios.get(
+        `${process.env.EXPO_PUBLIC_API_URL}/api/get-qr-code`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      return data as { token: string };
+    },
+  });
+  if (error) {
+    if (error instanceof AxiosError && error.response?.data.error) {
+      Alert.alert("Error", error.response.data.error);
+    } else {
+      Alert.alert("Error", error.message);
+    }
+  }
   return (
     <View style={tw`flex-1 justify-center items-center gap-y-7`}>
-      <View style={tw`gap-y-1.5 items-center`}>
-        <Text style={tw`text-2xl font-bold text-indigo-600`}>QR Code</Text>
-        <Text style={tw`text-rose-500`}>
-          Use this for marking offsite attendance!
-        </Text>
-      </View>
+      {isLoading ? (
+        <ActivityIndicator size={40} color={"#4F46E5"} />
+      ) : data && data.token ? (
+        <>
+          <View style={tw`gap-y-1.5 items-center`}>
+            <Text style={tw`text-2xl font-bold text-indigo-600`}>QR Code</Text>
+            <Text>Use this for marking offsite attendance!</Text>
+          </View>
 
-      <View style={tw`bg-white p-2`}>
-        <QRCode value={"XYZ"} size={200} />
-      </View>
+          <View style={tw`bg-white p-2`}>
+            <QRCode value={data.token} size={200} />
+          </View>
+        </>
+      ) : (
+        <Text style={tw`text-base text-center text-rose-600 font-semibold`}>
+          Some error occured. Please try again later!
+        </Text>
+      )}
     </View>
   );
 };
